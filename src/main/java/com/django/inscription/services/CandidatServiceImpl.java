3425d1f1cde7da7150db912a8d5735afec6bfb78
django123 <edougajean@gmail.com>
2022-05-21 09:35:19 +0200

    candidate CRUD service ok

--- src/main/java/com/django/inscription/services/CandidatServiceImpl.java
+++ src/main/java/com/django/inscription/services/CandidatServiceImpl.java
@@ -1,4 +1,59 @@
 package com.django.inscription.services;
 
-public class CandidatServiceImpl {
+import com.django.inscription.dto.CandidateDto;
+import com.django.inscription.entities.Candidate;
+import com.django.inscription.exceptions.ResourceNotFoundException;
+import com.django.inscription.mapper.CandidateMapper;
+import com.django.inscription.repositories.CandidateRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class CandidatServiceImpl implements CandidatService{
+
+    private final CandidateRepository candidateRepository;
+    private final CandidateMapper mapper;
+
+    @Override
+    public List<CandidateDto> listCandidate() {
+        List<Candidate> candidates = candidateRepository.findAll();
+        return candidates.stream()
+                         .map(mapper::fromCandidate)
+                         .collect(Collectors.toList());
+    }
+
+    @Override
+    public CandidateDto saveCandidate(CandidateDto candidateDto) {
+
+        Candidate candidate = mapper.fromCandidateDto(candidateDto);
+        Candidate saveCandidate = candidateRepository.save(candidate);
+        return mapper.fromCandidate(saveCandidate);
+    }
+
+    @Override
+    public CandidateDto updateCandidate(CandidateDto candidateDto) {
+        Candidate candidate = mapper.fromCandidateDto(candidateDto);
+        Candidate updateCandidate = candidateRepository.save(candidate);
+        return mapper.fromCandidate(updateCandidate);
+    }
+
+    @Override
+    public CandidateDto getCandidate(String candidateId) {
+        Candidate candidate = candidateRepository.findById(candidateId).orElseThrow(()->new ResourceNotFoundException("Candidate", "id",candidateId));
+        return mapper.fromCandidate(candidate);
+    }
+
+    @Override
+    public void deleteCandidate(String candidateId) {
+       Candidate candidate = candidateRepository.findById(candidateId).orElseThrow(()->new ResourceNotFoundException("Candidate", "id",candidateId));
+       candidateRepository.delete(candidate);
+    }
 }
